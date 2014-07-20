f9fe00ee043c72e27268a2bca0038f994f700fa4
Michael Bock <mykel.bock@gmail.com>
2014-07-19 21:45:50 -0700

    fixes

--- chat.js
+++ chat.js
@@ -1,10 +1,5 @@
 
 window.onload = function() {
-  var randInt = Math.random()*10000;
-  var room;
-  var me = {userId: '1', klassId: '200', klass: 'CSE 101', name: 'User'};
-
-  var socket = io.connect('http://ec2-54-186-60-145.us-west-2.compute.amazonaws.com:3456');
 
   console.log('running the onload thing');
   chrome.windows.getCurrent(function (currentWindow) {
@@ -15,58 +10,61 @@ window.onload = function() {
     });
   });
 
-  // Received from the server
-  socket.on('render_chat', function(content) {
-    // content : {room: room, content: 'some string'}
-    room = content.room;
-    console.log(content);
-    $("#messagesDiv").text("");
-    for (var i = 0; i < content.room.messages.length; ++i) {
-      displayChatMessage(content.room.messages[i].poster.name, content.room.messages[i].content,
-        content.room.messages[i].time);
-    }
-    // displayChatMessage(content.name, content.text);
-  });
+  chrome.extension.onRequest.addListener(function(info_to_send) {
+    console.log("chat.js received info: " + info_to_send);
 
-  // Sent to the server
-  function sendMessage(message) {
+    //var randInt = Math.random()*10000;
+    var room;
+    var me = {userId: '1', klassId: '200', klass: 'CSE 101', name: 'User'};
+
+    var socket = io.connect('http://ec2-54-186-60-145.us-west-2.compute.amazonaws.com:3456');
+
+
+    me.name = info_to_send.user_full_name;
+    me.userId = info_to_send.user_full_name; //+ randInt.toString();
+    $('#class_name').text(info_to_send.class_name);
+
+    socket.on('connect', function(){
+      socket.emit('init_message', me);
+    });
+
+    // Received from the server
+    socket.on('render_chat', function(content) {
+      // content : {room: room, content: 'some string'}
+      room = content.room;
+      console.log(content);
+      $("#messagesDiv").text("");
+      for (var i = 0; i < content.room.messages.length; ++i) {
+        displayChatMessage(content.room.messages[i].poster.name, content.room.messages[i].content,
+          content.room.messages[i].time);
+      }
+      // displayChatMessage(content.name, content.text);
+    });
+
+    // Sent to the server
+    function sendMessage(message) {
+      console.log('sendMessage ran');
       // terrible hack to wait till room is defined
       if (!room) {
+        console.log('no room');
         return;
       }
       socket.emit('send_message', {content: message, room: room, messageType: 'chat', poster: me});
-  }
-
-
-
-  // var myDataRef = new Firebase('https://qpv6vlip1d8.firebaseio-demo.com/');
-  $('#messageInput').keypress(function (e) {
-    if (e.keyCode == 13 && $("#messageInput").val() != "") {
-      var text = $('#messageInput').val();
-      sendMessage(text);
-      $('#messageInput').val('');
     }
-  });
-  // myDataRef.on('child_added', function(snapshot) {
-  //   var message = snapshot.val();
-  //   displayChatMessage(message.name, message.text);
-  // });
-  function displayChatMessage(name, text, time) {
-    $('<div/>').text(text + '(' + time + ')').prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  };
 
-  chrome.extension.onRequest.addListener(function(info_to_send) {
+    // var myDataRef = new Firebase('https://qpv6vlip1d8.firebaseio-demo.com/');
+    $('#messageInput').keypress(function (e) {
+      if (e.keyCode == 13 && $("#messageInput").val() != "") {
+        var text = $('#messageInput').val();
+        sendMessage(text);
+        $('#messageInput').val('');
+      }
+    });
 
-    console.log("chat.js received info: " + info_to_send);
-    me.name = info_to_send.user_full_name;
-    me.userId = info_to_send.user_full_name + randInt.toString();
-    $('#class_name').text(info_to_send.class_name);
+    function displayChatMessage(name, text, time) {
+      $('<div/>').text(text + '(' + time + ')').prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+    };
 
-    if (!room) {
-      socket.on('connect', function(){
-        socket.emit('init_message', me);
-      });
-    }
   });
 };
